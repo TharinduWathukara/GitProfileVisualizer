f0c62852f9ecd9275ba304d574736e4f1dfc59e4
Wathukarage Tharindu Gayan Wathukara <wtgwathnkara.15@cse.mrt.ac.lk>
2018-03-23 08:36:51 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,14 +1,14 @@
 const githubConfig ={
-    client_id : "ce6a0f1790fab5aa1799",
-    client_secret : "d8e45268b706ccff32ca6f788e710d9a263ac56f"
+    client_id : "client_id",
+    client_secret : "client_secret"
 }
 // this is for github authentication.
 
 const mongodbConfig={
-    username:"gpa",
-    password:"gitprofilevisualizer",
-    host:"ds059365.mlab.com",
-    port:"59365",
+    username:"username",
+    password:"password",
+    host:"host",
+    port:"port",
     dbname:"gpa"
 }
 //this is for mongodb authentication.
